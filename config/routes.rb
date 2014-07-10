9ff827d5d2bf4cb69141a281b264353e08c0ca1d
Abe Storey <abester101@gmail.com>
2014-07-10 15:54:48 -0700

    update show view and change root path

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Pinteresting::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'pages#home'
+   root "pinners#index"
   get "about" => "pages#about"
 
   # Example of regular route:
